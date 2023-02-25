e742bdb0e99099427b2e07f01be7d45a8d4cac08
mrcesar95 <cesaruxfxtx@gmail.com>
2023-02-25 17:06:52 -0500

    Uploading marvel-api for technical test

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Marvel-Api
+
+This is a project using  API de Marvel: https://developer.marvel.com/ to show their characters and information.
+
+In the project directory, you can run:
+
+### `npm install`
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
---
+++ marvel-api/src/Components/Main.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { Card } from "./Card"
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+export const Main = () => {
+  const [url,setUrl]=useState("https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=2e1cdeec426ae323484f29024084c206&hash=d516513ba95b9407c7aca0f73b241f8a")
+  const [item,setItem]=useState();
+  const [search,setSearch]=useState("");
+  useEffect(()=>{
+    const fetch=async()=>{
+      const res=await axios.get(url)
+      setItem(res.data.data.results);
+    }
+    fetch();
+  },[url])
+  
+  const searchMarvel=()=>{
+    setUrl(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&ts=1&apikey=2d7409ee03facbff7b861de50f1b33be&hash=bcd82b58b8ce1a5c0547f89429ad587c`)
+  }
+
+  return (
+    <>
+        <div className="header">
+            <div className="bg">
+                <img src="./Images/bg.png" alt="" />
+            </div>
+            <div className="search-bar">
+                <img src="./Images/logo1.png" alt="logo" />
+                <input type="search" placeholder='Search Here'
+                 className='search'
+                 onChange={e=>setSearch(e.target.value)}
+                 onKeyPress={searchMarvel}/>
+            </div>
+        </div>
+       <div className="content">
+         
+        {
+          (!item)?<p>Not Found</p>:<Card data={item}/>
+        }
+       </div>
+    </>
+  )
+}
